b461bef19d6e9fd6260ce7e0736cb0e807f59c2a
kimdongwon <ehddnjs8989@gmail.com>
2020-07-21 01:54:07 +0900

    Initial commit

---
+++ src/main/java/com/dongwon/book/Application.java
@@ -0,0 +1,17 @@
+package com.dongwon.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Application class 는 이 프로젝트의 메인 클래스.
+
+@SpringBootApplication
+// └ 역할 : 스프링 부트의 자동설정, 스프링 Bean읽기와 생성 모두 자동으로 설정.
+//          @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에
+//          이 클래스는 항상 프로젝트의 최상단에 위치해야하만 한다.
+public class Application {
+    public static void main(String[] args){
+        // SpringApplication.run으로 인해 내장 was가 자동 실행됨.
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/dongwon/book/web/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.dongwon.book.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+
+
+@WebMvcTest
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));    // mvc.perform의 결과를 검증
+        // 응답 본문의 내용을 검증
+        // Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는 검증한다.
+    }
+
+
+}
