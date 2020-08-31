5e173436c95773114e5aba50925a545fe0b6743e
kimdongwon <ehddnjs8989@gmail.com>
2020-09-01 00:49:43 +0900

    Login User 어노테이션 화 완료

--- src/main/java/com/dongwon/book/web/IndexController.java
+++ src/main/java/com/dongwon/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.dongwon.book.web;
 
+import com.dongwon.book.config.auth.LoginUser;
 import com.dongwon.book.config.auth.dto.SessionUser;
 import com.dongwon.book.domain.user.User;
 import com.dongwon.book.service.posts.PostsService;
@@ -20,10 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
